142033a6d637fc32a98aad1b4d0c08e6797382de
HarveyRun <timeline.1994.1976@gmail.com>
2022-09-19 15:32:41 +0800

    新增rem计算

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,15 +11,15 @@ export class HomeComponent implements OnInit {
   pageLimit = 20;
   public directionCount = 0;
   page = 0;
-  typeOption = { content: "该组件只支持Touch事件，请使用移动模式/设备打开此页。", marqueeProps: { fps: 100 } };
-  state = {
+  typeOption:any = { content: "该组件只支持Touch事件，请使用移动模式/设备打开此页。", marqueeProps: { fps: 100 } };
+  state:any = {
     refreshState: {
       currentState: 'deactivate',
       drag: false
     },
     direction: '',
     endReachedRefresh: false,
-    height: 500,
+    height: "auto",
     data: [],
     directionName: 'both up and down'
   };
@@ -29,30 +29,6 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.addItems(0);
-  }
-
-  onClick() {
-    this.directionCount++;
-    switch (this.directionCount) {
-      case 0:
-        this.state.direction = '';
-        this.state.directionName = 'both up and down';
-        break;
-      case 1:
-        this.state.direction = 'down';
-        this.state.endReachedRefresh = true;
-        this.state.directionName = 'down';
-        break;
-      case 2:
-        this.state.direction = 'up';
-        this.state.directionName = 'up';
-        break;
-      default:
-        this.directionCount = 0;
-        this.state.direction = '';
-        this.state.directionName = 'both up and down';
-        break;
-    }
   }
 
   pullToRefresh(event:any) {
@@ -81,6 +57,7 @@ export class HomeComponent implements OnInit {
 
   addItems(startIndex:any) {
     for (let i: number = startIndex; i < this.pageLimit * (this.page + 1); i++) {
+      this.state.data.push(i);
     }
   }
 
